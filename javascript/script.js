c4e23433f94b2fcf4e5b445f126b8c773dbcc0d9
Gisela <ghagedo@gmail.com>
2024-01-09 16:52:18 -0600

    random fixes

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,18 @@
 let openHam = document.querySelector('#openHam');
 let closeHam = document.querySelector('#closeHam');
 let navigationItems = document.querySelector('#navigation-items');
+var hamburger = document.getElementById('hamburger');
 
 const hamburgerEvent = (navigation, close, open) => {
-    navigationItems.style.display = navigation;
-    closeHam.style.display = close;
-    openHam.style.display = open;
+    console.log(hamburger);
+    if (window.getComputedStyle(hamburger).display === "none"){
+
+    }
+    else {
+        navigationItems.style.display = navigation;
+        closeHam.style.display = close;
+        openHam.style.display = open;
+    }
 };
 
 openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
